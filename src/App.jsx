b80ffaedc54c61e525fe15cb0deed3d334bf2383
kbot-sa <keanenjacobs2@gmail.com>
2023-11-25 13:11:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import WeatherApp from './Components/WeatherApp/WeatherApp'
+function App() {
+
+  return (
+    <div className='main'>
+      <WeatherApp/>
+    </div>
+      )
+}
+
+      export default App
